5377d925bb30e2150ca363ee641c7c9cac61f1f5
g-carla <giulia.carl9@gmail.com>
2020-05-11 10:10:14 +0200

    image_filter: add example and change input

--- tesi/image_filter.py
+++ tesi/image_filter.py
@@ -9,68 +9,76 @@ from ccdproc.image_collection import ImageFileCollection
 
 
 class ImageFilter():
-    '''
-    Select images from a directory depending on the filtering keys and create
-    a list of the selection as CCDData.
+    """
+    Select images from a directory depending on the filtering keys entered by
+    the user and create a list of the selection as CCDData.
 
     Parameters
     ----------
-    imasDirectory: path of the directory containing the files to be filtered.
-        Files must be in FITS format.
-    keys: FITS header's keywords that the user want to use for the filtering.
-    '''
+    files_path: str
+            path of the directory containing the files to be filtered.
+            Files must be in FITS format.
+    keys: list
+        FITS header's keywords that the user want to use for the
+        filtering.
 
-    def __init__(self, imasDirectory,
-                 keys=['FRAMETYP', 'OBJECT', 'DIT', 'FILTER',
-                       'OBJRA', 'OBJDEC', 'DATE-OBS', 'DATE']):
-        self._dir = imasDirectory
+    Example
+    -------
+    path = '/Users/giuliacarla/Documents/INAF/Lavoro/ARGOS/20161019'
+    keys = ['FRAMETYP', 'OBJECT', 'DIT', 'FILTER', 'OBJRA', 'OBJDEC',
+            'DATE-OBS', 'DATE']
+    ima_filter = ImageFilter(path, keys)
+    """
+
+    def __init__(self, files_path, keys):
+        self._path = files_path
         self._keys = keys
-        self._imagesCollection = None
+        self._images_collection = None
 
-    def getLists(self, frametyp, intTime, **kwargs):
-        '''
+    def getLists(self, **kwargs):
+        """
         Return a list of the selected images and a list of the corresponding
         filenames.
 
         Parameters
         ----------
-        frametyp: value of the header's keyword relative to the type of image
-            that the user want to select.
-            Example: frametyp='DARK' for dark frames
-                     frametyp='SCIENCE' for frame with the scientific target,
-                     etc.
-        intTime: value of the header's keyword relative to the exposure time
-            of images that the user want to select.
-        **kwargs: value of the other keywords available for the filtering.
+        **kwargs: keyword and its value which we want to use for the filtering.
 
         Returns
         -------
-        imaList: list of selected images as CCDData.
-        fileNameList: list of filenames relative to the selected images.
-        '''
+        imas_list: list
+                list of selected images as CCDData.
+        names_list: list
+                list of the filenames corresponding to the selected images.
 
-        self._createLists(frametyp, intTime, **kwargs)
-        return self.imaList, self.fileNameList
+        Example
+        ------- 
+        ngc_imas, ngc_names = ima_filter.getLists(FRAMETYP='SCIENCE',
+                                                  OBJECT='NGC2419'
+                                                  FILTER='J',
+                                                  DIT=3.)
+        """
+
+        self._createLists(**kwargs)
+        return self.imas_list, self.names_list
 
     def _createImageCollection(self):
-        self._imagesCollection = ImageFileCollection(self._dir, self._keys)
+        self._images_collection = ImageFileCollection(self._path, self._keys)
 
     def _getImageCollection(self):
-        if self._imagesCollection is None:
+        if self._images_collection is None:
             self._createImageCollection()
-        return self._imagesCollection
+        return self._images_collection
 
-    def _createLists(self, frameType, intTime, **kwargs):
-        self.imaList = []
-        self.fileNameList = []
+    def _createLists(self, **kwargs):
+        self.imas_list = []
+        self.names_list = []
         for ima, fname in self._getImageCollection().hdus(
-                FRAMETYP=frameType,
-                DIT=intTime,
                 return_fname=True,
                 **kwargs):
-            meta=ima.header
-            meta['filename']= fname
-            self.imaList.append((ccdproc.CCDData(data=ima.data,
-                                                 meta=meta,
-                                                 unit="adu")))
-            self.fileNameList.append(fname)
+            meta = ima.header
+            meta['filename'] = fname
+            self.imas_list.append((ccdproc.CCDData(data=ima.data,
+                                                   meta=meta,
+                                                   unit="adu")))
+            self.names_list.append(fname)
